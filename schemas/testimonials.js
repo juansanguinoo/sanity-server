d4a0095167f1dbf2c214407e6bfe737365c7a583
Juan David <juandavidsanguinoo@gmail.com>
2024-01-20 20:56:17 -0500

    Add schema types to index.js

---
+++ schemas/testimonials.js
@@ -0,0 +1,30 @@
+export default {
+  name: 'testimonials',
+  title: 'Testimonials',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'testimonial',
+      title: 'Testimonial',
+      type: 'text',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+}
